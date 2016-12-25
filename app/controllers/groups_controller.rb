c68fbdc2878e68183d70b0fd9ce75aa2dd1c12e5
ximigo1022 <liaohuatang1208@gmail.com>
2016-12-25 10:16:09 +0800

    implement post create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,6 +12,7 @@ end
 
 def show
   @group = Group.find(params[:id])
+  @posts = @group.posts
 end
 
 def edit
@@ -42,7 +43,7 @@ end
 
 
 def destroy
-  
+
   @group.destroy
 
   redirect_to groups_path, alert: "Group delete"
